b640ffd080ab7b65025a2697b27cf3869470e41d
Viktoriia Kozminska <kozminska.viktoriia@chnu.edu.ua>
2024-11-28 03:07:58 +0200

    first commit

--- User-Registration-Authentication-and-Authorization/src/main/java/DogMate/User/Registration/Authentication/and/Authorization/Repositories/AppUserRepositories.java
+++
@@ -1,9 +0,0 @@
-package DogMate.User.Registration.Authentication.and.Authorization.Repositories;
-
-import DogMate.User.Registration.Authentication.and.Authorization.Models.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepositories extends JpaRepository<AppUser, Integer> {
-
-    public AppUser findByUsername(String username);
-}
